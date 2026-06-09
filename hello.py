e2070af41596afa2d98f45535e31b5b84378f550
bavithrasivakumar21085-hue <bavithrasivakumar21085@gmail.com>
2026-06-09 14:48:04 +0530

    my program

--- hello.py
+++ hello.py
@@ -2,5 +2,5 @@ name="Bavithra"
 age= 24
 height=5.5
 is_intern=True
-print(f"I am {name} I'm {age} old, my height is {height}, am i a intern{is_intern} ")
+print(f"My name is {name} I'm {age} old, My height is {height}, am I an intern {is_intern} ")
 
