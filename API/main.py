0703ff25b51bc1c8bfb20ffd768edd352957eabb
bavithrasivakumar21085-hue <bavithrasivakumar21085@gmail.com>
2026-06-19 16:08:59 +0530

    Day 10 task completed

---
+++ API/main.py
@@ -0,0 +1,37 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+students = []
+
+class Student(BaseModel):
+    name:str
+    age:int
+
+@app.post("/students")
+def add_student(student:Student):
+    students.append(student)
+    return "new student added successfully"
+
+@app.get("/students")
+def get_student():
+    
+    return students
+
+@app.get("/students/{id}")
+def get_student(id:int):
+    return students[id]
+
+@app.put("/students/{id}")
+def update_student(id:int,student:Student):
+    students[id]=student
+    return "Updated Successfully"
+
+@app.delete("/students/{id}")
+def delete_student(id:int):
+    if id>=len(students):
+        return "Student not found"
+    students.pop(id)
+    return "Deleted Successfully"
+
