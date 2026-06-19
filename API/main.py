6464bdeb38e1fa2b9243e9afcb6e5722a880d5a9
bavithrasivakumar21085-hue <bavithrasivakumar21085@gmail.com>
2026-06-19 15:16:02 +0530

    day 10 task completed fastAPI learned

--- API/main.py
+++ API/main.py
@@ -1,12 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
-@app.get("/")
-def greeting():
-    return "Welcome JSON"
 
-@app.get("/hello/{name}")
-def test(name:str):
-    return f"hello. {name}, Welcome to FastAPI"
+students = []
 
+class Student(BaseModel):
+    name:str
+    age:int
+
+@app.post("/students")
+def add_student(student:Student):
+    students.append(student)
+    return "new student added successfully"
+
+@app.get("/students")
+def get_student():
+    
+    return students
+
+@app.get("/students/{id}")
+def get_student(id:int):
+    return students[id]
+
+@app.put("/students/{id}")
+def update_student(id:int,student:Student):
+    students[id]=student
+    return "Updated Successfully"
+
+@app.delete("/students/{id}")
+def delete_student(id:int):
+    if id>=len(students):
+        return "Student not found"
+    students.pop(id)
+    return "Deleted Successfully"
 
